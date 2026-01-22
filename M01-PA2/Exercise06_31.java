e4fa000cb412449d53e73a4d3b0a173696070f2e
yayaX-X <nadiariley41201@gmail.com>
2026-01-22 01:57:25 +0000

    create outline of program

---
+++ M01-PA2/Exercise06_31.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Exercise06_31 {
+    // get card number from user
+    Scanner input = new Scanner(System.in);System.out.print("Enter a credit card number as a long integer: ");
+    long number = input.nextLong();
+
+    // return true if the card number is valid
+    public static boolean isValid(long number){
+    
+    }
+
+    // get sum of single digits
+    public static int sumOfDoubleEvenPlace(long number){
+        
+    }
+
+    // return single digit number, otherwise sum
+    public static int getDigit(int number){
+        
+    }
+    // return sum of odd place digits
+    public static int sumOfOddPlace(long number){
+        
+    }
+    // return true is number d is a prefix for number
+    public static boolean prefixMatched(long number, int d){
+        
+    }
+    // return the number of digits in d
+    public static int getSize(long d){
+        
+    }
+    // return the first k number of digits from number. If the number of digits in
+    // number is less than k, return number
+    public static long getPrefix(long number, int k){
+        
+    }
+    // print whether the card number is valid or invalid (main method)
+    public static void main(String[] args) {
+
+    }
+}
