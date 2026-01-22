6d808634f12d95d982c6fbeb7ff022672f6ccd45
yayaX-X <nadiariley41201@gmail.com>
2026-01-22 03:05:23 +0000

    creating outline

---
+++ M01-PA3/Exercise08_29.java
@@ -0,0 +1,17 @@
+public class Exercise08_29 {
+    // prompt user to enter two 3 x 3 arrays
+    public static int[] getArray() {
+
+    }
+
+    // compare the two arrays
+    public static boolean equals(int[][] m1, int[][] m2) {
+
+    }
+
+    // display whether the two arrays are strictly identical
+    public static void main(String[] args) {
+
+    }
+
+}
