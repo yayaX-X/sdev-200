4cd1589c384b6a1b18c851693d8d6d9452d475bb
yayaX-X <nadiariley41201@gmail.com>
2026-01-28 19:45:05 +0000

    getting rid of error

--- M01-PA3/Exercise08_29.java
+++ M01-PA3/Exercise08_29.java
@@ -1,12 +1,12 @@
 public class Exercise08_29 {
     // prompt user to enter two 3 x 3 arrays
     public static int[] getArray() {
-
+        return new int[9];
     }
 
     // compare the two arrays
     public static boolean equals(int[][] m1, int[][] m2) {
-
+        return true;
     }
 
     // display whether the two arrays are strictly identical
