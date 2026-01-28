2b7f6f473cd1619e4fea2229747a7fa85ed729d8
yayaX-X <nadiariley41201@gmail.com>
2026-01-28 18:37:20 +0000

    code outline

---
+++ M02-PA1/MyDate.java
@@ -0,0 +1,42 @@
+import java.util.GregorianCalendar;
+
+// Class creation and declerations
+public class MyDate()
+{
+    int year;
+    int month;
+    int day;
+
+    // no-arg constructor for current date
+    public MyDate() {
+        
+    }
+
+    // constructor with elapsed time
+    public MyDate(long elapsedTime) {
+        
+    }
+
+    // constructor with specific date
+    public MyDate(int year, int month, int day) {
+        
+    }
+
+    // getters
+    public int getYear() {
+        return year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    // method to set date with elapsed time
+    public void setDate(long elapsedTime) {
+
+    }
+}
